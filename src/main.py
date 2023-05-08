d21b6e0614e36dc1a8426a5d5f2d5b117418a831
Ryan Hoban <rhoban03@gmail.com>
2023-05-08 18:38:56 +0100

    parallel api calls

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, url_for, request, redirect
 from nba_api.stats.static import players, teams
 from nba_api.stats.endpoints import playercareerstats
+from requests_futures.sessions import FuturesSession
 import requests
 import json
 import datetime
 import pandas as pd
-
 today = datetime.date.today()
 year = today.year
 
@@ -14,26 +14,33 @@ app = Flask(__name__)
 all_players = players.get_players()
 all_teams = teams.get_teams()
 
-def get_players_career(player_name):
+def get_season(bdl_id, season_year):
+    response = requests.get("https://www.balldontlie.io/api/v1/season_averages?player_ids[]=" + str(bdl_id) + "&season=" + str(season_year))
+    if response.status_code == 200:
+        if json.loads(response.content)["data"]:
+            season_to_add = json.loads(response.content)["data"][0]
+            season_to_add["season"] = f"{season_year}-{season_year+1}"
+            return season_to_add
+    return None
+
+
+def get_players_career_stats(player_name):
     # get player id from balldontlie api
     response = requests.get("https://www.balldontlie.io/api/v1/players?search=" + player_name)
     if response.status_code == 200:
-        bdl_id = json.loads(response)["data"][0]["id"]
+        bdl_id = json.loads(response.content)["data"][0]["id"]
     else:
         return None
-
-    # get player career stats from balldontlie api
-    stats = []
-    for season_year in range(1946, year):
-        response = requests.get("https://www.balldontlie.io/api/v1/season_averages?player_ids[]=" + bdl_id + "&season=" + str(season_year))
-        if response.status_code == 200:
-            if json.loads(response)["data"]:
-                to_add = json.loads(response)["data"][0]
-                to_add["season"] = f"{season_year}-{season_year+1}"
-                stats.append(to_add)
     
-    return stats
+    stats = []
 
+    with FuturesSession() as session:
+        futures = [get_season(bdl_id, season_year) for season_year in range(1996, year)]
+        for future in futures:
+            if future.result() is not None:
+                stats.append(future.result())
+
+    return stats
 
 @app.route("/")
 def home():
@@ -64,7 +71,7 @@ def player(player_id):
     player_name = [p for p in all_players if p['id'] == int(player_id)][0]['full_name']
     # career = playercareerstats.PlayerCareerStats(player_id=player_id, timeout=10)
     # player_info = career.get_data_frames()[0]
-    career_stats = get_players_career(player_name)
+    career_stats = get_players_career_stats(player_name)
     if career_stats:
         player_info = pd.DataFrame(career_stats)
         return render_template("player.html", player_name=player_name, player_info=player_info)
