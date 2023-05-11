3516a1db539e409c9cd43f806f438359a3a4a576
Ryan Hoban <rhoban03@gmail.com>
2023-05-11 19:54:57 +0100

    adding headers to request

--- src/main.py
+++ src/main.py
@@ -11,6 +11,16 @@ player_names = [player["full_name"] for player in all_players]
 all_teams = teams.get_teams()
 team_names = [team["full_name"] for team in all_teams]
 
+headers = {
+    'Host': 'stats.nba.com',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Referer': 'https://stats.nba.com/',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+}
+
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -39,7 +49,7 @@ def player_search():
 def player(player_id):
     player_name = [p for p in all_players if p['id'] == int(player_id)][0]['full_name']
     try:
-        career = playercareerstats.PlayerCareerStats(player_id=player_id, timeout=10)
+        career = playercareerstats.PlayerCareerStats(player_id=player_id, headers=headers, timeout=10)
         player_info = career.get_data_frames()[0]
         return render_template("player.html", player_name=player_name, player_info=player_info)
     except json.decoder.JSONDecodeError:
