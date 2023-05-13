e453c3e92ff31075130cd350ad70ab0ddccff077
Ryan Hoban <rhoban03@gmail.com>
2023-05-13 18:42:34 +0100

    altering user agent

--- src/nba_requests.py
+++ src/nba_requests.py
@@ -5,7 +5,7 @@ import json
 
 headers = {
     'Host': 'stats.nba.com',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
+#    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
     'Accept': 'application/json, text/plain, */*',
     'Accept-Language': 'en-US,en;q=0.5',
     'Referer': 'https://stats.nba.com/',
